5b5f914f9fe096d27f7d05266d2d90a50c994112
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-07 16:11:32 +0900

    Update formhier.cpp
    2/7 DS->cyonglee

--- formhier.cpp
+++ formhier.cpp
@@ -1,41 +1,38 @@
-// Copyright (C) 2017 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#include "formhier.h"
+#include "ui_formhier.h"
+#include "mainwindow.h"
+#include <QTableWidget>
+#include <QDebug>
 
-#include <QGuiApplication>
-#include <QVulkanInstance>
-#include <QLoggingCategory>
-#include "../shared/trianglerenderer.h"
 
-Q_LOGGING_CATEGORY(lcVk, "qt.vulkan")
-
-class VulkanWindow : public QVulkanWindow
+FormHier::FormHier(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FormHier)
 {
-public:
-    QVulkanWindowRenderer *createRenderer() override;
-};
-
-QVulkanWindowRenderer *VulkanWindow::createRenderer()
-{
-    return new TriangleRenderer(this, true); // try MSAA, when available
+    ui->setupUi(this);
 }
 
-int main(int argc, char *argv[])
+FormHier::~FormHier()
 {
-    QGuiApplication app(argc, argv);
+    delete ui;
+}
 
-    QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
+//void FormHier::testMyData(){
+//    this->dataset->split_datas[0][0] = QString("No, I'm not fool!!!");
+//}
 
-    QVulkanInstance inst;
-    inst.setLayers({ "VK_LAYER_KHRONOS_validation" });
+void FormHier::ReceiveSplitData()
+{
+    qDebug() << "xxxxxxxxxxxxxxxxxxxxxxxxx";
 
-    if (!inst.create())
-        qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
 
-    VulkanWindow w;
-    w.setVulkanInstance(&inst);
 
-    w.resize(1024, 768);
-    w.show();
 
-    return app.exec();
+    ui->tableWidget->setColumnCount(20);
+    //ui->tableWidget->setHorizontalHeaderLabels(split_data[100][100]);
+
+
+
+    //qDebug() << split_data;
+
 }
