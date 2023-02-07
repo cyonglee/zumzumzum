1a12107d82507b8478046c4c4203dc7a623c68fb
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-07 16:19:09 +0900

    Update formmap.h
    2/7 DS->cyonglee

--- formmap.h
+++ formmap.h
@@ -1,33 +1,22 @@
-// Copyright (C) 2017 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#ifndef FORMMAP_H
+#define FORMMAP_H
 
-#ifndef CAMERA_H
-#define CAMERA_H
+#include <QDialog>
 
-#include <QVector3D>
-#include <QMatrix4x4>
+namespace Ui {
+class FormMap;
+}
 
-class Camera
+class FormMap : public QDialog
 {
+    Q_OBJECT
+
 public:
-    Camera(const QVector3D &pos);
-
-    void yaw(float degrees);
-    void pitch(float degrees);
-    void walk(float amount);
-    void strafe(float amount);
-
-    QMatrix4x4 viewMatrix() const;
+    explicit FormMap(QWidget *parent = nullptr);
+    ~FormMap();
 
 private:
-    QVector3D m_forward;
-    QVector3D m_right;
-    QVector3D m_up;
-    QVector3D m_pos;
-    float m_yaw;
-    float m_pitch;
-    QMatrix4x4 m_yawMatrix;
-    QMatrix4x4 m_pitchMatrix;
+    Ui::FormMap *ui;
 };
 
-#endif
+#endif // FORMMAP_H
