ac939af2fd9a49edddfcaee378544f2adce2a357
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-07 16:20:25 +0900

    Update formtop.cpp
    2/7 DS->cyonglee

--- formtop.cpp
+++ formtop.cpp
@@ -1,41 +1,14 @@
-// Copyright (C) 2017 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+#include "formtop.h"
+#include "ui_formtop.h"
 
-#include <QGuiApplication>
-#include <QVulkanInstance>
-#include <QLoggingCategory>
-#include "../shared/trianglerenderer.h"
-
-Q_LOGGING_CATEGORY(lcVk, "qt.vulkan")
-
-class VulkanWindow : public QVulkanWindow
+FormTop::FormTop(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FormTop)
 {
-public:
-    QVulkanWindowRenderer *createRenderer() override;
-};
-
-QVulkanWindowRenderer *VulkanWindow::createRenderer()
-{
-    return new TriangleRenderer(this, true); // try MSAA, when available
+    ui->setupUi(this);
 }
 
-int main(int argc, char *argv[])
+FormTop::~FormTop()
 {
-    QGuiApplication app(argc, argv);
-
-    QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
-
-    QVulkanInstance inst;
-    inst.setLayers({ "VK_LAYER_KHRONOS_validation" });
-
-    if (!inst.create())
-        qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
-
-    VulkanWindow w;
-    w.setVulkanInstance(&inst);
-
-    w.resize(1024, 768);
-    w.show();
-
-    return app.exec();
+    delete ui;
 }
