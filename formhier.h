6e146681e3b87f4440c984a88206d03da714472b
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-08 18:44:32 +0900

    Update formhier.h
    2/8 ds>cyonglee

--- formhier.h
+++ formhier.h
@@ -1,115 +1,33 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef FORMHIER_H
+#define FORMHIER_H
 
-#include "./trianglerenderer.h"
+#include <QDialog>
+//#include "all_data.h"
+//#include "mainwindow.h"
 
-#include <QMainWindow>
-#include <QVulkanWindow>
-#include <QDockWidget>
-#include "formhier.h"
-#include "formlayer.h"
-#include "formtop.h"
-#include "forminfo.h"
-#include "formmap.h"
-#include "all_data.h"
+namespace Ui {
+class FormHier;
+}
 
-//#include "mydataset.h"
-
-class VulkanWindow;
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-
-
-class MainWindow : public QMainWindow
+class FormHier : public QDialog
 {
     Q_OBJECT
 
-public:
-    MainWindow(VulkanWindow *w);
-    ~MainWindow();
-
-
-
 public slots:
-
-    void inputStatus(QString text);
-    void on_actionOpen_file_triggered();
-
-//    void takeInfoValue(QString infoName, float value);
-
-private slots:
-    void on_pushButton_clicked();
-
-signals:
-    void sendInfoValue(float value);
-    void sendSelectFileName(QString file_name);
+    void ReceiveSplitData(QStringList list, int row, int column);
 
 public:
-    QVulkanWindow *m_window;
-    Ui::MainWindow *ui;
-    QString statusText;
-    FormHier *formHier;
-    FormLayer *formLayer;
-    FormTop *formTop;
-    FormInfo *formInfo;
-    FormMap *formMap;
-    all_data *donut;
-    QStringList split_data;
-
+    FormHier(QWidget *parent = nullptr);
+    ~FormHier();
+    Ui::FormHier *ui;
 
 public:
     //MyDataSet *dataset = nullptr;
 
 public:
     //void setMyDataSet(MyDataSet * dataset_){ this->dataset = dataset_;}
+    //void testMyData();
 
 };
 
-
-
-// Graphics output
-class VulkanRenderer : public TriangleRenderer
-{
-public:
-    VulkanRenderer(VulkanWindow *w);
-};
-
-// Graphics output -> Qt
-class VulkanWindow : public QVulkanWindow
-{
-    Q_OBJECT
-
-public:
-    QVulkanWindowRenderer *createRenderer() override;
-
-
-private:
-    VulkanRenderer *m_renderer;
-    void wheelEvent(QWheelEvent *) override;
-    void mousePressEvent(QMouseEvent *) override;
-    void mouseReleaseEvent(QMouseEvent *) override;
-    void mouseMoveEvent(QMouseEvent *) override;
-    void keyPressEvent(QKeyEvent *) override;
-    void keyReleaseEvent(QKeyEvent *) override;
-
-    bool keyCtrl = false;
-    bool keyShift = false;
-    bool keyAlt = false;
-
-    int m_mouseButton = 0;
-
-
-    QPoint m_lastPos;
-
-
-signals:
-    void outputStatus(QString funcValue);
-    void sendInfo(QString infoName, float value);
-};
-
-
-
-#endif // MAINWINDOW_H
+#endif // FORMHIER_H
