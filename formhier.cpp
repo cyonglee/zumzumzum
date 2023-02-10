e25461f0586f6b484fe4d06e118c737b1acbb5a5
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-10 15:33:24 +0900

    Update formhier.cpp
    2/10 DS>me

--- formhier.cpp
+++ formhier.cpp
@@ -10,6 +10,18 @@ FormHier::FormHier(QWidget *parent) :
     ui(new Ui::FormHier)
 {
     ui->setupUi(this);
+    // QTableWidget defalut 크기 설정
+    QStringList defalutTableHeader;
+
+    ui->tableWidget->setColumnCount(5);
+    ui->tableWidget->setRowCount(5);
+    ui->tableWidget->setColumnWidth(0,50);
+    ui->tableWidget->setColumnWidth(1,50);
+    ui->tableWidget->setColumnWidth(2,50);
+    ui->tableWidget->setColumnWidth(3,50);
+    ui->tableWidget->setColumnWidth(4,50);
+    ui->tableWidget->setColumnWidth(5,50);
+
 }
 
 FormHier::~FormHier()
@@ -21,24 +33,50 @@ FormHier::~FormHier()
 //    this->dataset->split_datas[0][0] = QString("No, I'm not fool!!!");
 //}
 
-void FormHier::ReceiveSplitData(QStringList list, int row, int column)
+//void FormHier::ReceiveSplitData(QStringList list, int row, int column,  QVector <QVector <QString>> &inputDataVector)
+void FormHier::ReceiveSplitData(int row, int column, const QVector <QVector <QString>> &inputDataVector)
 {
     qDebug() << "xxxxxxxxxxxxxxxxxxxxxxxxx";
-    qDebug() << &list;
-    qDebug() << list[19];
-    qDebug() << row;
-    qDebug() << column;
-//    qDebug() << myVector.value(1);
+    qDebug() << "row" << row;
+    qDebug() << "column" << column;
+    qDebug() << "1st" << inputDataVector.value(0);
+    qDebug() << "2nd" << inputDataVector.value(1);
+    qDebug() << "3rd" << inputDataVector.value(2);
+    qDebug() << "1st_1st" << inputDataVector.value(0).value(0);
+
+// QTableWidget 에서 QStringList 로만 받기때문에 자료형변환
+    QStringList vectorTOqstringlist;
+    QStringList vectorTOqstringlistHoriLabels;
+
+// QTableWidget 크기 설정
+    ui->tableWidget->setColumnCount(column);
+    ui->tableWidget->setRowCount(row);
+
+// Table Header Font size/bold change
+    QFont font = ui->tableWidget->horizontalHeader()->font();
+    font.setBold(true);
+    font.setPointSize(10);
+    ui->tableWidget->horizontalHeader()->setFont(font);
+
+// Table 채우기
+    for (int i=0; i<row ; i++)
+    {
+        for (int j=0; j<column ; j++)
+        {
+            vectorTOqstringlist << inputDataVector.value(i+1).value(j);
+            vectorTOqstringlistHoriLabels << inputDataVector.value(0).value(j);
+            ui->tableWidget->setItem(i,j,new QTableWidgetItem(vectorTOqstringlist[i*column+j]));
+            ui->tableWidget->setHorizontalHeaderLabels(vectorTOqstringlistHoriLabels);
+        }
+
+    }
+//    ui->tableWidget->setColumnWidth(0,80);
+//    ui->tableWidget->setColumnWidth(1,80);
+//    ui->tableWidget->setColumnWidth(2,80);
+//    ui->tableWidget->setColumnWidth(3,50);
+//    ui->tableWidget->setColumnWidth(4,50);
+//    ui->tableWidget->setColumnWidth(5,50);
 
 
-//    for ()
-
-
-    ui->tableWidget->setColumnCount(20);
-    //ui->tableWidget->setHorizontalHeaderLabels(split_data[10][10]);
-
-
-
-    //qDebug() << split_data;
 
 }
