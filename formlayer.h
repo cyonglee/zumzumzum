5bafca863d37c7c762b22d629563a571787cd887
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-15 16:15:52 +0900

    Update formlayer.h
    2/15 ds>me

--- formlayer.h
+++ formlayer.h
@@ -2,6 +2,10 @@
 #define FORMLAYER_H
 
 #include <QDialog>
+#include <QtWidgets/QCheckBox>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QCheckBox>
 
 namespace Ui {
 class FormLayer;
@@ -11,9 +15,17 @@ class FormLayer : public QDialog
 {
     Q_OBJECT
 
+public slots:
+    void ReceiveSplitData(int row, int column, const QVector <QVector <QString>> &inputDataVector);
+
 public:
     explicit FormLayer(QWidget *parent = nullptr);
     ~FormLayer();
+//    QTableWidgetItem *checkBoxItem[3];
+
+
+private slots:
+    void on_tableWidget_itemChanged(QTableWidgetItem *item);
 
 private:
     Ui::FormLayer *ui;
