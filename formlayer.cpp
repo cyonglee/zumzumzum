21e03b1243f7dc36e114efd260edaaddb0fbb85f
cyonglee <121470059+cyonglee@users.noreply.github.com>
2023-02-24 17:14:43 +0900

    Update formlayer.cpp
    2/24 ds -> me

--- formlayer.cpp
+++ formlayer.cpp
@@ -1,6 +1,5 @@
 #include "formlayer.h"
 #include "ui_formlayer.h"
-#include <QDebug>
 
 
 
@@ -10,17 +9,15 @@ FormLayer::FormLayer(QWidget *parent) :
 {
     ui->setupUi(this);
     QStringList defalutTableHeader;
-    defalutTableHeader << "" << "LayerName" << "LayerNum" << "LayerType";
+    defalutTableHeader << "" << "color" << "LayerName" << "LayerNum" << "LayerType";
 
-    ui->tableWidget->setColumnCount(4);
+    ui->tableWidget->setColumnCount(5);
     ui->tableWidget->setRowCount(10);
-    ui->tableWidget->setColumnWidth(0,15);
+    ui->tableWidget->setColumnWidth(0,12);
     ui->tableWidget->setColumnWidth(1,80);
     ui->tableWidget->setColumnWidth(2,80);
     ui->tableWidget->setHorizontalHeaderLabels(defalutTableHeader);
-
     ui->tableWidget->horizontalHeader()->setStyleSheet("QHeaderView::section {background-color:#404040;color:#FFFFFF;}");
-
 
 
 }
@@ -30,27 +27,28 @@ FormLayer::~FormLayer()
     delete ui;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief FormLayer::ReceiveSplitData
+/// \param row
+/// \param column
+/// \param inputDataVector
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void FormLayer::ReceiveSplitData(int row, int column, const QVector <QVector <QString>> &inputDataVector)
 {
-//    qDebug() << "xxxxxxxxxxxxxxxxxxxxxxxxx";
-//    qDebug() << "row" << row;
-//    qDebug() << "column" << column;
-//    qDebug() << "1st" << inputDataVector.value(0);
-//    qDebug() << "2nd" << inputDataVector.value(1);
-//    qDebug() << "3rd" << inputDataVector.value(2);
-//    qDebug() << "1st_1st" << inputDataVector.value(0).value(0);
-
-
-
 // QTableWidget 에서 QStringList 로만 받기때문에 자료형변환
     QStringList vectorTOqstringlist;
     QStringList vectorTOqstringlistHoriLabels;
+    QStringList vectorTOqstringcolorlist;
+    QString LayerColorRed;
+    QString LayerColorGreen;
+    QString LayerColorBlue;
+    QString LayerColorText;
     vectorTOqstringlistHoriLabels << " ";
 
 // QTableWidget 크기 설정
-    ui->tableWidget->setColumnCount(column-2);
-    ui->tableWidget->setRowCount(row);
+    ui->tableWidget->setColumnCount(column-1);
+    ui->tableWidget->setRowCount(row-1);
 
 // Table Header Font size/bold change
     QFont font = ui->tableWidget->horizontalHeader()->font();
@@ -58,54 +56,105 @@ void FormLayer::ReceiveSplitData(int row, int column, const QVector <QVector <QS
     font.setPointSize(10);
     ui->tableWidget->horizontalHeader()->setFont(font);
 
-
-    // Table 채우기
+// Table 채우기
     for (int i=0; i<row ; i++)
     {
-        // checkbox 채우기
-        QTableWidgetItem *checkBoxItem = new QTableWidgetItem();
-        checkBoxItem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
-        checkBoxItem->setCheckState(Qt::Checked);
-
-//        QTableWidget::itemC
-
-
-        ui->tableWidget->setItem(i,0,checkBoxItem);
-
+        // table value 채우기
         for (int j=0; j<(column-3) ; j++)
         {
             vectorTOqstringlist << inputDataVector.value(i+1).value(j);
-            vectorTOqstringlistHoriLabels << inputDataVector.value(0).value(j);
-            ui->tableWidget->setItem(i,j+1,new QTableWidgetItem(vectorTOqstringlist[i*(column-3)+j]));
+//            vectorTOqstringlistHoriLabels << inputDataVector.value(0).value(j+1);
+            ui->tableWidget->setItem(i,j+2,new QTableWidgetItem(vectorTOqstringlist[i*(column-3)+j]));
+            qDebug() << "column : " << column;
         }
 
+        // checkbox 채우기
+//        QTableWidgetItem *checkBoxItem = new QTableWidgetItem();
+//        checkBoxItem->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
+//        checkBoxItem->setCheckState(Qt::Checked);
+//        ui->tableWidget->setItem(i,0,checkBoxItem);
+//        ui->tableWidget->setFocusPolicy(Qt::NoFocus);
+
+
+        QCheckBox *checkBoxItem = new QCheckBox();
+        checkBoxItem->setCheckState(Qt::Checked);
+        QWidget *checkboxWidget = new QWidget();
+        QHBoxLayout *checkboxLayout = new QHBoxLayout(checkboxWidget);
+        checkboxLayout->addWidget(checkBoxItem);
+        checkboxLayout->setAlignment(Qt::AlignCenter);
+        checkboxLayout->setContentsMargins(0,0,0,0);
+        checkboxWidget->setLayout(checkboxLayout);
+        ui->tableWidget->setCellWidget(i,0,checkboxWidget);
+        QObject::connect(checkBoxItem, SIGNAL(clicked()), this, SLOT(on_tableWidget_itemChanged()));
+
+        // Layer color rgb value
+        LayerColorRed = inputDataVector.value(i+1).value(3);
+        LayerColorGreen = inputDataVector.value(i+1).value(4);
+        LayerColorBlue = inputDataVector.value(i+1).value(5);
+
+        LayerColorText = "background-color: rgb(" + LayerColorRed;
+        LayerColorText = LayerColorText + "," + LayerColorGreen + "," + LayerColorBlue + ")";
+
+        // color pushbutton 채우기
+        QPushButton *colorbutton = new QPushButton();
+        colorbutton->setStyleSheet(LayerColorText);
+        ui->tableWidget->setCellWidget(i,1,colorbutton);
+        QObject::connect(colorbutton, SIGNAL(clicked()), this, SLOT(on_colorbutton_clicked()));
     }
-
     ui->tableWidget->setHorizontalHeaderLabels(vectorTOqstringlistHoriLabels);
-
-
-    QObject::connect(this,SIGNAL(&QTableWidget::itemChaged),this,SLOT(on_tableWidget_itemChanged));
+//    ui->tableWidget->resizeColumnsToContents();
 
 }
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief FormLayer::on_tableWidget_itemChanged
+/// \param checkBoxItem
+/// description : checkbox 선택에 따라 Layer On/Off mainwindow status 창에 출력
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-void FormLayer::on_tableWidget_itemChanged(QTableWidgetItem *checkBoxItem)
+void FormLayer::on_tableWidget_itemChanged()
 {
-    int clickedRow = checkBoxItem->row();
-
+    int clickedRow = ui->tableWidget->currentRow();
     qDebug() << clickedRow;
-
-    QTableWidgetItem *item = ui->tableWidget->item(clickedRow,1);
+    QTableWidgetItem *item = ui->tableWidget->item(clickedRow,2);
 
     if (item != nullptr) {
-       QString cellValue = item->text();
-        qDebug() << "Cell value at (1, 1):" << cellValue;
-    } else {
-        qDebug() << "No item at (1, 1)";
-    }
+        QString selectedLayer = item->text();
+        QString printLayer = "selectedLayer = " + selectedLayer;
 
+        bool LayerOnOff = ui->tableWidget->cellWidget(clickedRow,0)->windowState();
+        qDebug() << LayerOnOff;
+        if (LayerOnOff==true){
+            printLayer = printLayer + "  Layer  On";
+            emit outputLayerStatus(printLayer);
+            qDebug() << printLayer;
+        } else {
+            printLayer = printLayer + "  Layer  Off";
+            emit outputLayerStatus(printLayer);
+            qDebug() <<  printLayer;
+        }
+
+    } else {
+        qDebug() << "No item";
+    }
+}
+
+void FormLayer::on_colorbutton_clicked()
+{
+    QString LayerColorname;
+    QString LayerColorText;
+    int colorbuttonRow = ui->tableWidget->currentRow();
+
+    QColor color = QColorDialog::getColor(Qt::yellow, this);
+
+    LayerColorname = color.name();
+    LayerColorText = "background-color: " + LayerColorname;
+
+//    qDebug() << ui->tableWidget->cellWidget(colorbuttonRow,1);
+
+    ui->tableWidget->cellWidget(colorbuttonRow,1)->setStyleSheet(LayerColorText);
 
 }
+
